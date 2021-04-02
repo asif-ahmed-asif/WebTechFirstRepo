e0a7ff9790f502208ccbcca32fa5e9352beaec46
Asif <asifahmed1132@gmail.com>
2021-04-02 22:44:44 +0600

    Lab task 7 uploaded

--- Lab Task 6/Login.php
+++ Lab Task 6/Login.php
@@ -11,9 +11,9 @@
 		<fieldset>
 			<legend><b>LOGIN</b></legend>
 			<label>User ID:</label>
-			<input type="text" name="uid" value="<?php if (isset($_COOKIE["userid"])){echo $_COOKIE["userid"];}?>"><br>
+			<input type="text" name="uid" required value="<?php if (isset($_COOKIE["userid"])){echo $_COOKIE["userid"];}?>"><br>
     		<label>Password:</label>
-    		<input type="password" name="password" value="<?php if (isset($_COOKIE["password"])){echo $_COOKIE["password"];}?>"><br>
+    		<input type="password" name="password" required value="<?php if (isset($_COOKIE["password"])){echo $_COOKIE["password"];}?>"><br>
     		<input type="checkbox" name="remember" <?php if (isset($_COOKIE["userid"]) && isset($_COOKIE["password"])){ echo "checked";}?>>Remember Me<br><br>
     		<input type="submit" name="submit" value="Submit">
     		<a href="ForgetPassword.php">Forgot Password?</a>
